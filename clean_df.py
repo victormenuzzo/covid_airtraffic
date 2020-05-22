0cc75de6aa28fbae1080f696c963dbe1c6583a1b
Victor Antonio Menuzzo <49654865+victormenuzzo@users.noreply.github.com>
2020-05-22 13:42:36 -0300

    code cleaned

--- clean_df.py
+++ clean_df.py
@@ -1,66 +1,41 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
 
 def clean(df):
     df = df[['icao24','lastseen','departure', 'destination']]    
     df = df.dropna()
     return df
 
-month_list = ['jan','fev', 'mar', 'abr', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
-df = pd.DataFrame()
+month_list = ['jan', 'fev', 'mar', 'abr', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
+
 for i in month_list:
     df_aux =  pd.read_csv('data/'+i+'2018.csv')
     df_aux = clean(df_aux)
     df = df.append(df_aux)
-
 df.head()
 
-
-# In[4]:
-
-
+#Loading aircraft model
 aircraft = pd.read_csv('data/aircraftDatabase.csv')
 aircraft = aircraft[['icao24', 'model']]
-
-icao24_appe = df.icao24.unique()
 cods_aircraft = aircraft.icao24
-cods_aircraft_notin = list(set(icao24_appe) - set(cods_aircraft))
 aircraft = aircraft.set_index('icao24')
-
 aircraft = aircraft.to_dict()
 
+#Converting airport initials to country
 airports = pd.read_csv('data/airports.csv')
 airport_country = airports[['cod_icao', 'pais']]
 cods_airports = airports.cod_icao
 airport_country = airport_country.set_index('cod_icao')
-#USAGE: airport_country['cod_icao']
 airport_country = airport_country.to_dict()
 
-
-df['model'] = df['icao24'].apply(lambda x: aircraft['model'][x] if x not in cods_aircraft_notin else None)
-
-df['pais_destino'] = df['destination'].apply(lambda x: airport_country['pais'][x] if x in cods_airports.values else None)
-
+#Adding origin country 
 df['pais_origem'] = df['departure'].apply(lambda x: airport_country['pais'][x] if x in cods_airports.values else None)
 
+#Adding destination country
+df['pais_destino'] = df['destination'].apply(lambda x: airport_country['pais'][x] if x in cods_airports.values else None)
 
-# In[5]:
-
+#Adding aircraft model
+df['model'] = df['icao24'].apply(lambda x: aircraft['model'][x] if x in cods_aircraft.values else None)
 
 df = df[['icao24','lastseen', 'pais_origem', 'pais_destino', 'model']]
-
-
-# In[6]:
-
-
 df.to_csv('filghts_2018_model.csv', index=False)
 
